486befbf1bcf0d7701bb0d8adc63ea93d33356f9
Shivangi Garg <shivangi.garg@uwaterloo.ca>
2023-07-24 05:41:34 -0400

    async-storage scaffolding

--- App.tsx
+++ App.tsx
@@ -9,15 +9,19 @@ import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
-import store from './src/redux/store';
+import { store, persistor } from './src/redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
 import BottomTabNav from './src/navigation/bottomTabNavigation';
+import { Text } from "react-native";
 
 function App(): JSX.Element {
   return (
     <Provider store={store}>
+      <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
       <NavigationContainer>
         <BottomTabNav />
       </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,7 +1,15 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
 import {configureStore} from '@reduxjs/toolkit';
 
 import {reducer} from './reducer';
+import storage from 'redux-persist/lib/storage'
 
-const store = configureStore({reducer: reducer});
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, reducer);
+export const store = configureStore({reducer: persistedReducer});
+export const persistor = persistStore(store);
